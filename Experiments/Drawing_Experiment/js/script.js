0fae2c0bb3c2c7720bf7aa58cfde4402a818baab
EmileJolicoeur <125163394+EmileJolicoeur@users.noreply.github.com>
2023-09-17 14:31:19 -0400

    Troubleshooting Alien

--- Experiments/Drawing_Experiment/js/script.js
+++ Experiments/Drawing_Experiment/js/script.js
@@ -2,8 +2,7 @@
  * Drawing_Experiment
  * Emile Jolicoeur
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Making an alien out of shapes in JavaScript!
  */
 
 "use strict";
@@ -20,43 +19,45 @@ function preload() {
  * Description of setup
 */
 function setup() {
+
     createCanvas(500, 500);
     background(0, 0, 0);
     let r = random(0, 255);
-    
 
 //Head Mantle Shape
-
     noStroke();
     fill(40, 40, 100);
     ellipse(250, 240, 450);
+    ellipse(250, 240, 450);
     fill(0, 0, 0);
     rect(0, 250, 500, 260);
-    
-//Shoulders
 
+//Shoulders
     rectMode(CENTER);
+    fill(200, 200, 200);
+    triangle(200, 400, 250, 150, 300, 400);
     fill(50, 70, 100);
     rect(250, 430, 450, 140, 100, 100, 0, 0);
-    //rect(250, 300, 50, 200);
-    
 
 //Head
-
     ellipseMode(CENTER);
     ellipse(250, 220, 175);
     fill(0, 0, 0);
     ellipse(160, 290, 100);
     ellipse(340, 290, 100);
-    fill(200, 200, 200);
-    triangle(30, 75, 58, 20, 86, 75);
+    noFill();
+    strokeWeight(5);
+    stroke(30, 0, 50);
+    bezier(165, 200, 200, 190, 300, 190, 335, 200);
+    
 //FaceDetails
     noFill();
     strokeWeight(5);
-    stroke(200, 0, 50);
+    stroke(30, 0, 50);
     bezier(165, 200, 200, 190, 300, 190, 335, 200);
     strokeWeight(2);
     bezier(200, 260, 200, 190, 300, 190, 300, 260);
+
 }
 
 
@@ -65,9 +66,16 @@ function setup() {
 */
 function draw() {
 
-    fill(random(0,255), random(0,255), random(0,255));
+ fill(random(0,255), random(0,255), random(0,255));
     ellipse(250, 200, 50);
+    bezier(200, 75, 265, 45, 235, 45, 300, 75);
+    bezier(200, 75, 265, 105, 235, 105, 300, 75);
 
-    bezier(200, 100, 200, 190, 300, 190, 300, 100);
+//EyeMovement
+    fill(0, 0, 0);
+    noStroke();
+    translate(mouseX*0.025, mouseY*0.025);
+    circle(250, 200, 10);
+    circle(250, 75, 10);
 
 }
