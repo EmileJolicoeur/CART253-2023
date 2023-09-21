934a35629a8a0430bd4c8dd6a27c7c283c571b69
EmileJolicoeur <125163394+EmileJolicoeur@users.noreply.github.com>
2023-09-21 02:30:02 -0400

    James bond intro Start (map video)

--- Experiments/Variables_Experiments/js/script.js
+++ Experiments/Variables_Experiments/js/script.js
@@ -10,10 +10,21 @@
 
 //Declaring & Assigning values to variables:
 let backgroundShade = 0;
+
+/**
 let circleX;
 let circleY;
 let circleSize = 100;
+let circleSpeed = 1;
+*/
 
+let circle = {
+    x: 0,
+    y: 0,
+    size: 100,
+    speed: 5,
+    fill: 255,
+};
 
 /**
  * Description of preload
@@ -28,14 +39,10 @@ function preload() {
 */
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    circleX = width / 2;
-    circleY = height / 2;
+    circle.x = 0;
+    circle.y = height / 2;
 }
 
-
-/**
- * Description of draw()
-*/
 function draw() {
 /** Video 1
     background(0, 255, 0);
@@ -44,8 +51,41 @@ function draw() {
 */
 
 /** Video 2: */
-    background(backgroundShade);
-    ellipse(circleX, circleY, circleSize);
+    background(backgroundShade, 0, 0);
+    ellipseMode(CENTER);
 
+    circle.x += circle.speed;
     
+    circle.fill = map(mouseY, 0, height, 0, 255);
+    fill(circle.fill, 0, 0);
+    ellipse(circle.x, circle.y, circle.size);
+
+    if (circle.x >= width) {
+        circle.speed = 0;
+        circle.size *= 1.03;
+        console.log(`Hit 1`);
+        
+        if (circle.size >= height / 2) {
+            circle.speed = -3;
+            circle.x += circle.speed;
+            console.log(`Hit 2`);
+        }
+        
+        if(circle.x <= width / 2) {
+            circle.speed = 0;
+            console.log(`Hit 3`);    
+        }
+    }
+
+/**Debugging
+    console.log("circleX: " + circle.x);
+    console.log("circleY: " + circle.y);
+    */
+//or...
+    console.log(`circleX: ${circle.x}, circleY: ${circle.y}, circleSize: ${circle.size}, circleSpeed: ${circle.speed}`);
+
+/**Random nb. variable:
+let randomNumber = random();
+console.log(randomNumber);
+*/
 }
