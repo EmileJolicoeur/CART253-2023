d0fabe0ce3725bcfdf3dced5eb51ac6d76b5bd09
EmileJolicoeur <125163394+EmileJolicoeur@users.noreply.github.com>
2023-09-20 23:33:35 -0400

    Defined and Assigned values to Variables

--- Experiments/Variables_Experiments/js/script.js
+++ Experiments/Variables_Experiments/js/script.js
@@ -1,12 +1,19 @@
 /**
- * Title of Project
- * Author Name
+ * Variables Experiment
+ * Emile Jolicoeur
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
  */
 
 "use strict";
+
+//Declaring & Assigning values to variables:
+let backgroundShade = 0;
+let circleX;
+let circleY;
+let circleSize = 100;
+
 
 /**
  * Description of preload
@@ -20,7 +27,9 @@ function preload() {
  * Description of setup
 */
 function setup() {
-
+    createCanvas(windowWidth, windowHeight);
+    circleX = width / 2;
+    circleY = height / 2;
 }
 
 
@@ -28,5 +37,15 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+/** Video 1
+    background(0, 255, 0);
+    rectMode(CENTER);
+    rect(width / 2, height / 2, -mouseX, -mouseY);
+*/
 
+/** Video 2: */
+    background(backgroundShade);
+    ellipse(circleX, circleY, circleSize);
+
+    
 }
