4d0bf871fc003c629de9816568f9df3944ab2329
EmileJolicoeur <125163394+EmileJolicoeur@users.noreply.github.com>
2023-09-21 22:09:40 -0400

    I-Like-To-Move-It

--- Experiments/I-Like-To-Move-It/js/script.js
+++ Experiments/I-Like-To-Move-It/js/script.js
@@ -13,7 +13,6 @@
 
 let backgroundShade = 0;
 
-
 //spotlight
 let circle = {
     x:      0,
@@ -29,37 +28,67 @@ function preload() {
 
 }
 
-
 // Just a canvas taking the whole screen
 function setup() {
 
     createCanvas(windowWidth, windowHeight);
     circle.x = 0;
-    circle.y = height / 2;
+    circle.y = height / 2; 
 }
 
 //
 function draw() {
 
+    //Restraint variables:
+    let leftWall = width /3;
+    let rightWall = 2*width /3;
+
+    //rectangle
+    let rectangle = {
+        x: width / 2,
+        y: height / 50,
+        h: 20,
+        fill: mouseX
+    };
+    let rectangleW = constrain(mouseX, leftWall, rightWall);
+
+    //Randomized triangle
+    let tri = {
+        x: 0,
+        y: 0,
+        fill: 255,
+    };
+
     //Setting the Background
     background(backgroundShade, 0, 0);
+
+    //Making a randomized triangle
+    tri.x = random(0, width);
+    tri.y = random(0, height);
+
+    fill(tri.fill, 0, tri.fill);
+    triangle(tri.x, height / 3, 4*width / 7, 2*height / 5, 50, tri.y);
     
+    //Rectangle
+    fill(0, rectangle.fill, 0);
+    rectMode(CENTER);
+    rect(rectangle.x, rectangle.y, rectangleW, rectangle.h);
 
     circle.x += circle.speed;
-    /** to change*/circle.fill = map(mouseY, 0, height, 0, 255);
+    circle.fill = map(mouseY, 0, height, 0, 255);
 
-    //creating the Spotlight
+    //creating the JamesBond Spotlight
     noStroke();
     ellipseMode(CENTER);
     fill(255, circle.fill, circle.fill);
-    ellipse(circle.x, circle.y,circle.size);
-
+    ellipse(circle.x, circle.y, circle.size);
+    
     //Moving the Spotlight
     if (circle.x >= width) {
         circle.speed = 0;
         circle.size *= 1.03;
         circle.hit = circle.hit+1
-        console.log(`CircleHit ${circle.hit}`)
+        console.log(`CircleHit ${circle.hit}`);
     
         //Transition from Spotlight to GunBarrel
         if (circle.size >= height / 2) {
@@ -71,47 +100,5 @@ function draw() {
     //If the barrel is on it's way back, stop at the center of the screen
     if (circle.x <= width / 2 && circle.hit > 0) {
         circle.speed = 0;
-
-        console.log(`Awaiting Input`);
-        backgroundShade = 1;
     }
-
-    //Shooting the shot
-    if (mousePressed === true && circle.hit >0) {
-        console.log(`Bang!`);
-        backgroundShade *= 50;
-    }
-
-
-    /** looping through the wall
-    if (circle.x > width)   {
-        circle.x = 0;
-    }   else    {
-        console.log(`Wall Breach!`);
-    }
-    */
-
-    //circle.size = constraint(mouseX, 0, width);
-    //mapping mouseX from the width 255 to 200 
-    //circle.fill = map(mouseX, 0, width, 255, 200);
-
-//Casino icons
-    //Diamonds:
-/**
-let quad ={
-    x1: 50,
-    y1: 62,
-    x2: 86,
-    y2: 50,
-    x3: 50,
-    y3: 38,
-    x4: 14,
-    y4: 50,
-
-
-}
-    quad(quad.x1, quad.y1, quad.x2, quad.y2, quad.x3, quad.y3, quad.x4, quad.y4);
-*/
-
-
 }
