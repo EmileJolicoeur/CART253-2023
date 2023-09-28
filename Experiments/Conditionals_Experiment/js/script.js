ffd5c240bd86d94f498bd4908aece3c24c78fa18
EmileJolicoeur <125163394+EmileJolicoeur@users.noreply.github.com>
2023-09-28 16:53:25 -0400

    Completion of conditionals exercises (1-8)

--- Experiments/Conditionals_Experiment/js/script.js
+++ Experiments/Conditionals_Experiment/js/script.js
@@ -1,32 +1,37 @@
 /**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Conditionals_Experiments
+ * Emile Jolicoeur
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
+let angle = 0;
+let rectScale   =   0;
 
-}
+//
+function preload() {}
 
-
-/**
- * Description of setup
-*/
+//Canvas
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 
-/**
- * Description of draw()
-*/
+//
 function draw() {
-
+    background(137);
+    
+    push();
+    fill(255, 0, 0);
+    rectMode(CENTER);
+    translate(width/2, height/2);
+    rotate(angle);
+    scale(2);
+    rect(0, 0, 100, 100);
+    pop();
+    
+    //rotationSpeed
+    angle   +=    0.01;
+    rectScale   +=  0.01;
 }
+
---
+++ Experiments/Dodge-em/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Exercise: Dodge-em
+ * Emile Jolicoeur
+ * 
+ * Objectives: Understanding following script conditions:
+ * if-statements, loops(drawing), mouse interaction, displaying images
+ */
+
+"use strict";
+
+//
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+
+}
+
+
+//
+function draw() {
+
+}
